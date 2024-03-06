5bec8eb7bf63a8319092d1ee4295dc40f34f034b
Dmitry Drigota <drigota@gmail.com>
2024-03-06 18:45:59 +0000

    Update feed page, added data from store

--- src/components/order-summary/order-summary.tsx
+++ src/components/order-summary/order-summary.tsx
@@ -1,34 +1,47 @@
+import { useAppSelector } from "../../utils/hooks";
 import styles from "./order-summary.module.css";
 
 function OrderSummary() {
+  const orders = useAppSelector(store => store.feed.data);
+  const readyOrders = orders?.orders.filter(order => order.status === "done");
+  const inProgressOrders = orders?.orders.filter(order => order.status !== "done");
+
   return (
     <div className={styles.summary__container}>
       <div className={styles.status}>
         <div className={styles.status_ready}>
-          <p className="text text_type_main-default mb-2">Готовы:</p>
+          <p className="text text_type_main-default mb-6">Готовы:</p>
           <div className={styles.ready}>
-            <p className={`${styles.ready_order} text text_type_digits-default`}>123123</p>
-            <p className={`${styles.ready_order} text text_type_digits-default`}>123123</p>
-            <p className={`${styles.ready_order} text text_type_digits-default`}>123123</p>
+            {readyOrders?.map(order => (
+              <p
+                key={order._id}
+                className={`${styles.ready_order} text text_type_digits-default`}>
+                {order.number}
+              </p>
+            ))}
           </div>
         </div>
         <div className={styles.status_in_progress}>
-          <p className="text text_type_main-default mb-2">В работе:</p>
+          <p className="text text_type_main-default mb-6">В работе:</p>
           <div className={styles.in_progress}>
-            <p className={`${styles.progress_order} text text_type_digits-default`}>123123</p>
-            <p className={`${styles.progress_order} text text_type_digits-default`}>123123</p>
-            <p className={`${styles.progress_order} text text_type_digits-default`}>123123</p>
+            {inProgressOrders?.map(order => (
+              <p
+                key={order._id}
+                className={`${styles.progress_order} text text_type_digits-default`}>
+                {order.number}
+              </p>
+            ))}
           </div>
         </div>
       </div>
 
       <div className={styles.alltime}>
-        <p className="text text_type_main-default">Выполнено за все время:</p>{" "}
-        <p className={`${styles.shadow} text text_type_digits-large`}>123456</p>
+        <p className="text text_type_main-default">Выполнено за все время:</p>
+        <p className={`${styles.shadow} text text_type_digits-large`}>{orders?.total}</p>
       </div>
       <div className={styles.today}>
         <p className="text text_type_main-default">Выполнено за сегодня:</p>
-        <p className={`${styles.shadow} text text_type_digits-large`}>123</p>
+        <p className={`${styles.shadow} text text_type_digits-large`}>{orders?.totalToday}</p>
       </div>
     </div>
   );
